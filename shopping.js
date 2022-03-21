aaad8352dbe7305e1d029aebd7b92f90ca4c65e4
bsnun8 <bishnunath018@gmail.com>
2022-03-21 20:26:40 +0600

    Dynamically set and read from local, session storage

---
+++ shopping.js
@@ -0,0 +1,37 @@
+const addItem = () => {
+  const nameField = document.getElementById('product-name');
+  const name = nameField.value;
+  nameField.value = '';
+  // display in the ui
+  displayProduct(name);
+  
+  // add to local storage
+  addProductToCart(name);
+}
+
+const displayProduct = name => {
+  const ul = document.getElementById('products');
+  const li = document.createElement('li');
+  li.innerText = name;
+  ul.appendChild(li);
+}
+
+const getCart = () => {
+  const cart = localStorage.getItem('cart');
+  let cartObj;
+  if (cart) {
+    cartObj = JSON.parse(cart);
+  } 
+  else {
+    cartObj = {};
+  }
+  return cartObj;
+}
+
+const addProductToCart = name => {
+  const cart = getCart();
+  cart[name] = 1;
+  const cartStringified = JSON.stringify(cart);
+  localStorage.setItem('cart', cartStringified);
+  console.log(cart);
+}
